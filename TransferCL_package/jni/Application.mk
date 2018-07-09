976317684246c0f77769bb74fed9bd03d852d1b4
bulletcross <bulletcross@gmail.com>
2018-07-09 21:41:41 +0530

    bug fixes and successful builds

---
+++ TransferCL_package/jni/Application.mk
@@ -0,0 +1,7 @@
+APP_STL := gnustl_static
+APP_CPPFLAGS := -frtti -fexceptions
+APP_OPTIM := release
+#APP_OPTIM := debug
+APP_ABI := armeabi-v7a armeabi
+APP_PLATFORM := android-14
+APP_CFLAGS := --std=c++11
